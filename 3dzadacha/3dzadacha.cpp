b8bc57d2184861e8dd7d568b2ba0106db61cee20
Валерия Каширина ПК <Aronilo@mail.ru>
2021-10-10 21:31:59 +0300

    Добавьте файлы проекта.

---
+++ 3dzadacha/3dzadacha.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double x, y, z;
+
+    cout << "x: ";
+    cin >> x;
+    cout << "y: ";
+    cin >> y;
+    cout << "z: ";
+    cin >> z;
+
+    int answer;
+
+    bool isInSphere = x * x + y * y + z * z < 1,
+        isInCylinder = sqrt((pow(z - y, 2) + pow(x - z, 2)
+            + pow(y - x, 2)) / 3) < 0.2;
+
+    if (isInSphere && isInCylinder) {
+        answer = 4;
+    }
+    else if (isInSphere && !isInCylinder) {
+        answer = 3;
+    }
+    else if (!isInSphere && isInCylinder) {
+        answer = 2;
+    }
+    else {
+        answer = 1;
+    }
+
+    cout << answer << endl;
+    return 0;
+}
